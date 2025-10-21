96eaf53c46c21ffec18091be87572eb8b0c9f2b5
cesar jan karlo <leossuarez103@gmail.com>
2025-10-21 17:41:42 -0600

    Se el componete boton

--- src/layout/Usuario/loginLayout.js
+++ src/layout/Usuario/loginLayout.js
@@ -1,5 +1,7 @@
 //layout de login
-import { View, Text, TextInput, Pressable, StyleSheet } from "react-native";
+import { View, Text, TextInput, StyleSheet } from "react-native";
+import { Boton } from "../../Componetes/boton";
+
 export function LoginLayout({ codigo, nip, onCodigoChange, onPasswordChange, onSubmit, error }) {
 return (
     <View style={styles.container}>
@@ -18,9 +20,11 @@ return (
             onChangeText={onPasswordChange}
         />
         {error ? <Text style={styles.error}>{error}</Text> : null}
-        <Pressable style={styles.button} onPress={onSubmit}>
-            <Text style={styles.buttonText}>Entrar</Text>
-        </Pressable>
+        <Boton 
+            title="Entrar"
+            color="#2563eb"
+            onPress={onSubmit}
+        />
     </View>
 );
 }
@@ -29,7 +33,5 @@ const styles = StyleSheet.create({
     container: { padding: 30, justifyContent: "center", flex: 1 },
     title: { fontSize: 24, textAlign: "center", marginBottom: 20 },
     input: { borderWidth: 1, borderRadius: 8, padding: 10, marginBottom: 10 },
-    button: { backgroundColor: "#2563eb", padding: 12, borderRadius: 8 },
-    buttonText: { color: "white", textAlign: "center", fontWeight: "bold" },
     error: { color: "red", textAlign: "center" },
 });
