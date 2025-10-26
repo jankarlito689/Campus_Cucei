b0bbc64058677e934008a977b547d431932b170d
cesar jan karlo <leossuarez103@gmail.com>
2025-10-25 23:32:15 -0600

    separamos el json en uno solo

--- src/Services/Auth/authService.js
+++ src/Services/Auth/authService.js
@@ -5,12 +5,24 @@ export async function login(codigo, nip) {
         const response = await apiCucei.get("auth.php",{
             params:{ codigo,nip}
         })
-        const data = response.data;
-        //validamos la informacion que nos llega
-        if (data.message || !data.codigo){
-            throw new Error(data.message || "Credenciales invalidas");
+        let raw = response.data;
+        //separamos el json
+        if(typeof raw === "string"){
+            const parts = raw.split("}{");
+            if(parts.length === 2){
+                raw = JSON.parse(parts[0] + "}")//primer objeto
+                const extra = JSON.parse("{"+ parts[1]) //segundo objeto
+                raw ={ ...raw, ...extra};
+            }else{
+                raw = JSON.parts(raw)
+            }
         }
-        return data;
+        //console.log("🔍 Respuesta completa del servidor:", raw);
+        //validamos la informacion que nos llega
+        if (!raw || !raw.codigo){
+            throw new Error("Credenciales invalidas");
+        }
+        return raw;
     } catch (error){
         if(error.message && !error.response && !error.request){
             throw error;
