1e2901aeeecc9ce4e03d7375e6df8ff77ba01708
cesar jan karlo <leossuarez103@gmail.com>
2025-10-26 16:18:34 -0600

    se agrego log de depuracion

--- src/Hooks/useAuth.js
+++ src/Hooks/useAuth.js
@@ -1,4 +1,8 @@
 import { AuthContext } from "../Handler/Auth/authProvider";
 import { useContext } from "react";
 //Hook para acceder al contexto
-export const useAuth = () => useContext(AuthContext)
+export const useAuth = () => {
+    const ctx = useContext(AuthContext);
+    //console.log("Contexto:", ctx)
+    return ctx;
+}
