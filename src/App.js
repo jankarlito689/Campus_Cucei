d29b405537e01856704636fd1f7c406cf2f610be
cesar jan karlo <leossuarez103@gmail.com>
2025-10-26 22:33:55 -0600

    se refactorizo la navegacion

--- src/App.js
+++ src/App.js
@@ -1,32 +1,10 @@
-import { LoginScreen } from "./Screen/Login";
-import { Mapa } from "./Screen/Mapa";
-import { MenuScreen } from "./Screen/Menu";
 import { AuthProvider } from "./Handler/Auth/authProvider";
-import { useAuth } from "./Hooks/useAuth";
-import { useState } from "react";
-
-function AppNavigator(){
-  const { user, signOut } = useAuth();
-  const [route, setRoute] = useState("menu")
-
-  if (!user){
-    return <LoginScreen/>
-  }
-  console.log("✅ Usuario autenticado:", user);
-  switch (route){
-    case "menu":
-      return <MenuScreen onNavigate={setRoute} onLogout={signOut}/>
-    case "mapa":
-      return <Mapa onBack={() => setRoute("menu")} />
-    default:
-      return <MenuScreen onNavigate={setRoute} onLogout={signOut}/>
-  }
-}
+import { Navigation } from "./Navigation";
 
 export default function App() {
   return (
     <AuthProvider>
-      <AppNavigator/>
+      <Navigation/>
     </AuthProvider>
   );
 }
