0cbe7aee0fc9f92595a1b529c7efb083485574d0
cesar jan karlo <leossuarez103@gmail.com>
2025-10-26 21:52:24 -0600

    se agrego la navegacion

--- src/App.js
+++ src/App.js
@@ -3,12 +3,24 @@ import { Mapa } from "./Screen/Mapa";
 import { MenuScreen } from "./Screen/Menu";
 import { AuthProvider } from "./Handler/Auth/authProvider";
 import { useAuth } from "./Hooks/useAuth";
+import { useState } from "react";
 
 function AppNavigator(){
-  const { user } = useAuth();
-  if (user) console.log("✅ Usuario autenticado:", user);
-  console.log({ LoginScreen, Mapa, MenuScreen });
-  return user ? <MenuScreen/> : <LoginScreen />
+  const { user, signOut } = useAuth();
+  const [route, setRoute] = useState("menu")
+
+  if (!user){
+    return <LoginScreen/>
+  }
+  console.log("✅ Usuario autenticado:", user);
+  switch (route){
+    case "menu":
+      return <MenuScreen onNavigate={setRoute} onLogout={signOut}/>
+    case "mapa":
+      return <Mapa onBack={() => setRoute("menu")} />
+    default:
+      return <MenuScreen onNavigate={setRoute} onLogout={signOut}/>
+  }
 }
 
 export default function App() {
