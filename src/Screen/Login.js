481c1fb8b7d33bab25894372782ba183d565ee90
cesar jan karlo <leossuarez103@gmail.com>
2025-11-06 14:57:14 -0600

    Se modifico el import del layout

--- src/Screen/Login.js
+++ src/Screen/Login.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { LoginLayout } from "../layout/Usuario/loginLayout";
+import { LoginLayout } from "../layout/Usuario/Auth/loginLayout";
 import { useAuth } from "../Hooks/useAuth";
 
 //Funcion que renderiza el layout 
