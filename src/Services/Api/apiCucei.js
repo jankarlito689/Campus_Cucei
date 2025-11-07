4ea590efbaf0fe95445b7ef4a46b6ef567273281
cesar jan karlo <leossuarez103@gmail.com>
2025-11-06 19:42:49 -0600

    se quito los headers y timeout

--- src/Services/Api/apiCucei.js
+++ src/Services/Api/apiCucei.js
@@ -3,6 +3,5 @@ import axios from "axios";
 //Creamos la funcion para conectarse al api de cucei
 export const apiCucei = axios.create({
     baseURL:"https://cuceimobile.space/campusCucei/",
-    timeout: 10000,
-    headers: { "Content-Type": "application/json"},
+    timeout: 0,
 });
