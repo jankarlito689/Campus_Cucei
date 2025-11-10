f825074ac2dae31ee623cfdbb2a09fd832d4f958
cesar jan karlo <leossuarez103@gmail.com>
2025-11-09 23:02:38 -0600

    se refactorizo el contenedor de map

--- src/Screen/Mapa.js
+++ src/Screen/Mapa.js
@@ -1,41 +1,22 @@
 import { useState } from 'react';
-import { View, Text, Image, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import { Boton } from '../Componetes/boton';
-import { useAuth } from '../Hooks/useAuth';
+import { MapaContent } from '../Componetes/mapaContent';
 //Importamos la zona interactiva
-import { ZonaInteractiva } from '../Componetes/zonaInteractiva';
 import { ModalHandle } from '../Handler/modalHandle';
-import { zonas } from '../Services/zonaService';
 
 export function Mapa({ onBack }) {
   // Estado que guarda qué módulo está activo
   const [activo, setActivo] = useState(null);
-
   return (
     <View style={styles.screen}>
       <Text style={styles.titulo}>Mapa</Text>
-
       <Boton 
         title='Volver a menu'
         color='#115688ff'
         onPress={onBack}
       />
-      {/* CONTENEDOR DEL MAPA */}
-      <View style={styles.mapContainer}>
-        <Image
-          source={require("../../assets/Img/mapa_2024.jpg")}
-          style={styles.map}
-        />
-          {/* ZONAS INTERACTIVAS */}
-        {zonas.map((zona) => (
-          <ZonaInteractiva
-            key={zona.id}
-            id={ zona.id}
-            setActivo={ setActivo}
-            style={zona.style}
-          />
-        ))}
-      </View>
+      <MapaContent setActivo={setActivo} />
       <ModalHandle activo={activo} onClose={() => setActivo(null)} />
     </View>
   );
@@ -56,30 +37,4 @@ const styles = StyleSheet.create({
     textAlign: "center",
     letterSpacing: 0.5,
   },
-  mapContainer: {
-    borderRadius: 20,
-    width: 550,
-    //aspectRatio: 8 / 8,
-    height: 650,
-    position: "relative", // necesario para usar absolute en las zonas
-    overflow: "hidden",
-    shadowColor: "#000",
-    shadowOffset: { width: 0, height: 4},
-    shadowOpacity:0.2,
-    shadowRadius:8,
-    elevation: 5,
-    backgroundColor: "#fff",
-  },
-  map: {
-    width: 590,
-    height: 650,
-    transform: [{ rotate: "90deg" }], // rota el mapa si tu imagen lo necesita
-    borderRadius: 20,
-    resizeMode: "contain",
-  },
-
-  zonePressed:{
-    backgroundColor: "rgba(0,0,0,0,.15)",
-  },
 });
-
