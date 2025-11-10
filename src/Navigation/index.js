bb4753a8b8db702f89a8efdded826d0a7510d029
cesar jan karlo <leossuarez103@gmail.com>
2025-11-09 19:20:50 -0600

    se cambio la navegacion a publica(map)

--- src/Navigation/index.js
+++ src/Navigation/index.js
@@ -9,26 +9,39 @@ import { AlumnoScreen } from "../Screen/Alumno";
 const Routes ={
     Menu: "menu",
     Mapa: "mapa",
-    Alumno: "alumno"
+    Alumno: "alumno",
+    Login: "login",
 }
 
 export function Navigation(){
     const { user, signOut } = useAuth();
     const [route, setRoute] = useState(Routes.Menu)
 
-    if (!user){
-        return <LoginScreen/>
-    }
     // VERIFICA que data sea un objeto y no contenga strings problemáticos
     //console.log("Datos de login:", user);
     switch (route){
         case Routes.Menu:
-            return <MenuScreen onNavigate={setRoute} onLogout={signOut}/>
+            return <MenuScreen onNavigate={setRoute}/>
         case Routes.Mapa:
             return <Mapa onBack={() => setRoute(Routes.Menu)} />
         case Routes.Alumno:
-            return <AlumnoScreen onBack={() => setRoute(Routes.Menu)} />
+            if (!user){
+                return(
+                    <LoginScreen
+                        onSuccess={() => setRoute(Routes.Alumno)}
+                        onBack={() => setRoute(Routes.Menu)}
+                    />
+                );
+            }
+            return <AlumnoScreen onBack={() => setRoute(Routes.Menu)}  onLogout={signOut}/>
+        case Routes.Login:
+            return(
+                <LoginScreen 
+                    onSuccess={() => setRoute(Routes.Menu)}
+                    onBack={() => setRoute(Routes.Menu)}
+                />
+            )
         default:
-            return <MenuScreen onNavigate={setRoute} onLogout={signOut}/>
+            return <MenuScreen onNavigate={setRoute} />
     }
 }
