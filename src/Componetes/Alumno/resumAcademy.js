1579a5755bc70881c12c337df5a3ef5019324431
cesar jan karlo <leossuarez103@gmail.com>
2025-11-23 00:21:19 -0600

    se agregaron las graficas

--- src/Componetes/Alumno/resumAcademy.js
+++ src/Componetes/Alumno/resumAcademy.js
@@ -1,21 +1,33 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Dimensions } from "react-native";
+import { CreeditosChart } from "../Graficas/creditosChart";
+import { GaugePromedio } from "../Graficas/gaugeProm";
 
 export function ResumAcademy({user}){
+    //Creeditos de user
+    const creditosCursados = Number(user?.creditos ?? 0) || 0;
+    const creditosTotales = Number(user?.creditosRequeridos ?? 0) || 0;
+    const promedio = Number(user?.promedio ?? 0) || 0;
+
     return(
         <View style={styles.section}>
             <Text style={styles.sectionTitle}>Resumen académico</Text>
                 <View style={styles.card}>
                     <Text style={styles.itemText}>Créditos:
-                        <Text style={styles.highlight}> {user.creditos}</Text> / {user.creditosRequeridos}
+                        <Text style={styles.highlight}>{creditosCursados}</Text> / {creditosTotales}
                     </Text>
                     <Text style={styles.itemText}>Promedio:
                         <Text style={styles.highlight}> {user.promedio}</Text>
                     </Text>
                     <Text style={styles.itemText}>Certificado: {user.certificado}</Text>
                 </View>
+                <Text style={styles.chartLabel}>Avance de creditos</Text>
+                <CreeditosChart creditosCursados={creditosCursados} creditosTotales={creditosTotales} />
+                <Text style={styles.chartLabel}>Tu promedio general</Text>
+                <GaugePromedio value={promedio} size={230}/>
         </View>
     )
 }
+
 const styles = StyleSheet.create({
     section: { 
         marginTop: 10 
@@ -42,4 +54,11 @@ const styles = StyleSheet.create({
         fontWeight: "bold", 
         color: "#115688" 
     },
+    chartLabel:{
+        margin:10,
+        marginBottom: 5,
+        fontSize: 16,
+        fontWeight:"bold",
+        color: "#115688"
+    }
 });
